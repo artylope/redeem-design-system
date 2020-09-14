7c62599aac3037ad28fbeae4153a87fa29626a3d
yixin <angyixin@gmail.com>
2020-09-15 06:00:04 +0800

    add bootstrap grid to compile dynmically

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log('connected');
+const template = document.createElement('template');
+template.innerHTML = 
+    <style>
+
+    </style>
+    <div class="tooltip-container">
+        test
+    </div>
+;
+
+
+
+class PopupNotify extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    }
+}
+
+window.customElements.define('popup-notify', PopupNotify);
